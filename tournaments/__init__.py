7480558ad390b899ae475eaabd7628d86b137d14
envasquez <vasquez.ericn@gmail.com>
2019-01-04 12:33:14 -0600

    starting over

--- tournaments/__init__.py
+++
@@ -1,4 +0,0 @@
-LAKES = [
-    'lbj', 'travis', 'belton', 'decker', 'canyon', 'medina', 'austin',
-    'lady-bird', 'stillhouse', 'richland-chambers', 'buchannan', ''
-]
--- users/views.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.shortcuts import render
-
-
-def index(request):
-    """Landing page"""
-    return render(request, 'index.html', {'title': 'South Austin Bass Club'})
