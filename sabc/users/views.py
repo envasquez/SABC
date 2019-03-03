5d337d6a951c9de5f77e659159d9a51b40a9e644
envasquez <vasquez.ericn@gmail.com>
2019-03-03 17:23:00 -0600

    Undo results app thing

--- sabc/users/views.py
+++ sabc/users/views.py
@@ -1,75 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-import json
-
 from django.contrib import messages
 from django.shortcuts import render, redirect
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.auth.decorators import login_required
-
-from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 from .models import Profile
 
-from tournaments.forms import TournamentForm
-from tournaments.models import Tournament
-
-
-class TournamentListView(ListView):
-    model = Tournament
-    ordering = ['-date'] # Newest tournament first
-    paginate_by = 1
-    template_name = 'users/index.html'
-    context_object_name = 'tournaments'
-
-
-class TournamentDetailView(DetailView):
-    model = Tournament
-
-
-class TournamentCreateView(SuccessMessageMixin, LoginRequiredMixin, CreateView):
-    model = Tournament
-    form_class = TournamentForm
-    success_message = 'Tournament Successfully Created!'
-
-    def form_valid(self, form):
-        form.instance.created_by = self.request.user.profile
-
-        return super(CreateView, self).form_valid(form)
-
-    def test_func(self):
-        return self.request.user.profile.type == 'officer'
-
-class TournamentUpdateView(SuccessMessageMixin, LoginRequiredMixin, UserPassesTestMixin, UpdateView):
-    model = Tournament
-    form_class = TournamentForm
-    success_message = 'Tournament Successfully Updated!'
-
-    def form_valid(self, form):
-        form.instance.updated_by = self.request.user.profile
-
-        return super(UpdateView, self).form_valid(form)
-
-    def test_func(self):
-        return self.request.user.profile.type == 'officer'
-
-
-class TournamentDeleteView(SuccessMessageMixin, LoginRequiredMixin, UserPassesTestMixin, DeleteView):
-    model = Tournament
-    success_message = 'Tournament [ %s ] Successfully Deleted!'
-    success_url = '/'
-
-    def test_func(self):
-        return not self.get_object().complete
-
-    def delete(self, request, *args, **kwargs):
-        obj = self.get_object()
-        messages.success(self.request, self.success_message % obj.__dict__.get('name', ''))
-
-        return super(TournamentDeleteView, self).delete(request, *args, **kwargs)
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 
 def about(request):
