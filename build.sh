472ca10653a65e9511bbad38bb7d6647b2b21e2a
Eric Vasquez <vasquez.ericn@gmail.com>
2022-07-30 10:43:25 -0500

    Remove volumes & db in docker, to reduce debug frustration for now

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@ set -e
 
 find . -name "*.pyc" -type f -delete
 find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
+find . -name "db.sqlite3" -type f -delete
 
 docker-compose down
+docker volume rm sabc_sabc-app
 docker-compose up -d --build --force-recreate sabc
