46a68ebeb95946a44115e49c798207f0d8fa8532
Eric Vasquez <vasquez.ericn@gmail.com>
2022-08-22 12:55:39 -0500

    Add IncorrectTournamentType exception

--- sabc/tournaments/exceptions.py
+++ sabc/tournaments/exceptions.py
@@ -3,3 +3,7 @@
 
 class TournamentNotComplete(Exception):
     """Exception for trying to do Result operations for a tournament still in progress"""
+
+
+class IncorrectTournamentType(Exception):
+    """Exception for trying to do team operations on a non-team tournament"""
