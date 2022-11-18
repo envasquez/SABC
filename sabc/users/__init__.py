5ee1ed2be769a70fd68636181b3f5517ca7df768
Eric Vasquez <vasquez.ericn@gmail.com>
2022-11-18 09:10:25 -0600

    reorder club officers

--- sabc/users/__init__.py
+++ sabc/users/__init__.py
@@ -14,9 +14,9 @@ CLUB_PRESIDENT = "president"
 CLUB_VICE_PRESIDENT = "vice-president"
 CLUB_SECRETARY = "secretary"
 CLUB_TREASURER = "treasurer"
-CLUB_TECHNOLOGY = "technology"
 CLUB_TOURNAMENT_DIRECTOR = "tournament-director"
 CLUB_ASSISTANT_TOURNAMENT_DIRECTOR = "assistant-tournament-director"
+CLUB_TECHNOLOGY = "technology"
 
 CLUB_OFFICERS_TYPES = (
     (CLUB_PRESIDENT, "president"),
