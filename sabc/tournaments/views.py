bf6c24e1d39771873cad9cebd61c05f94bc037e5
Eric Vasquez <vasquez.ericn@gmail.com>
2022-11-25 16:08:41 -0600

    Cleanup views code, remove docstrings for now

--- sabc/tournaments/views.py
+++ sabc/tournaments/views.py
@@ -1,8 +1,4 @@
 # -*- coding: utf-8 -*-
-# Using a file level pylint disable because of Django objects
-# pylint: disable=unused-argument
-"""Tournament Views"""
-from __future__ import unicode_literals
 
 from django.contrib import messages
 from django.views.generic import (
@@ -33,8 +29,6 @@ OFFICERS = [
 # Tournaments
 #
 class TournamentListView(ListView):
-    """Tournament list view"""
-
     model = Tournament
     ordering = ["-date"]  # Newest tournament first
     paginate_by = 3
@@ -43,25 +37,11 @@ class TournamentListView(ListView):
 
 
 class ExtraContext:
-    """Additional tournament data - namely results for a completed tournament"""
-
     extra_context = {}
 
-    def get_context_data(self, **kwargs):
-        """Override of get_context_data
-
-        Args:
-            kwargs (dict): Keyword arguments
-
-        Returns:
-            context (dict): Updated tournament context
-        """
-        context = super().get_context_data(**kwargs)
-        #
-        # Get the tournament results if any
-        #
+    def get_context_data(self):
+        context = super().get_context_data()
         tmnt = Tournament.objects.get(id=self.get_object().id)
-        Tournament.results.set_places(tournament=tmnt)
         if tmnt.points:
             Tournament.results.set_points(tournament=tmnt)
         self.extra_context["results"] = ResultTable(
@@ -70,9 +50,6 @@ class ExtraContext:
         self.extra_context["team_results"] = TeamResultTable(
             TeamResult.objects.filter(tournament=tmnt).order_by("place_finish")
         )
-        #
-        # Add the Tournament payout info
-        #
         self.extra_context["payouts"] = {}
         for key, val in Tournament.results.get_payouts(tmnt).items():
             self.extra_context["payouts"][key] = val
@@ -84,26 +61,16 @@ class ExtraContext:
 
 
 class TournamentDetailView(ExtraContext, DetailView):
-    """Tournament detail view"""
-
     model = Tournament
     context_object_name = "tournament"
 
 
 class TournamentCreateView(SuccessMessageMixin, LoginRequiredMixin, CreateView):
-    """Tournament create view"""
-
     model = Tournament
     form_class = TournamentForm
     success_message = "Tournament Successfully Created!"
 
-    def form_valid(self, form):
-        """Form validator"""
-        # pylint: disable=useless-super-delegation
-        return super().form_valid(form)
-
     def test_func(self):
-        """Test function for values"""
         return any(
             [
                 self.request.user.angler.officer_type in OFFICERS,
@@ -115,19 +82,15 @@ class TournamentCreateView(SuccessMessageMixin, LoginRequiredMixin, CreateView):
 class TournamentUpdateView(
     SuccessMessageMixin, LoginRequiredMixin, UserPassesTestMixin, UpdateView
 ):
-    """Tournament update view"""
-
     model = Tournament
     form_class = TournamentForm
     success_message = "Tournament Successfully Updated!"
 
     def form_valid(self, form):
-        """Form validator"""
         form.instance.updated_by = self.request.user.angler
         return super().form_valid(form)
 
     def test_func(self):
-        """Test function for values"""
         return any(
             [
                 self.request.user.angler.officer_type in OFFICERS,
@@ -139,18 +102,19 @@ class TournamentUpdateView(
 class TournamentDeleteView(
     SuccessMessageMixin, LoginRequiredMixin, UserPassesTestMixin, DeleteView
 ):
-    """Tournament delete view"""
-
     model = Tournament
     success_message = "Tournament [ %s ] Successfully Deleted!"
     success_url = "/"
 
-    def test_func(self):  # Don't delete tournaments that are not over
-        """Test function for values"""
-        return not self.get_object().complete
+    def test_func(self):
+        return any(
+            [
+                self.request.user.angler.officer_type in OFFICERS,
+                self.request.user.is_staff,
+            ]
+        )
 
     def delete(self, request, *args, **kwargs):
-        """Execute a deletion"""
         obj = self.get_object()
         messages.success(self.request, self.success_message % obj.__dict__.get("name", "UNKNOWN"))
         return super().delete(request, *args, **kwargs)
@@ -160,57 +124,60 @@ class TournamentDeleteView(
 # Results
 #
 class ResultCreateView(SuccessMessageMixin, LoginRequiredMixin, CreateView):
-    """Result creation view"""
-
     model = Result
     form_class = ResultForm
-    success_message = "Results Added for: %s"
 
-    def get_initial(self, *args, **kwargs):
-        """Get initial result"""
-        initial = super().get_initial(**kwargs)
+    def get_initial(self):
+        initial = super().get_initial()
         initial["tournament"] = self.kwargs.get("pk")
-
         return initial
 
-    def save(self, *args, **kwargs):
-        """Save a result - Trigger a re-setting of places and points when a result is saved"""
-        tmnt = Tournament.objects.get(self.get_initial(*args, **kwargs)["tournament"])
-        Tournament.results.set_places(tournament=tmnt)
-        if tmnt.points:
-            Tournament.results.set_points(tournament=tmnt)
-
-        messages.success(self.request, self.success_message % self.get_object())
-
     def test_func(self):
-        """Test function for values"""
         return any(
             [
                 self.request.user.angler.officer_type in OFFICERS,
                 self.request.user.is_staff,
             ]
         )
+
+    def form_valid(self, form):
+        tid = self.kwargs.get("pk")
+        duplicate = form.instance.angler in [
+            r.angler for r in Result.objects.filter(tournament=tid)
+        ]
+        if duplicate:
+            result = Result.objects.get(tournament=tid, angler=form.instance.angler)
+            messages.error(self.request, message=f"ERROR Result exists! {result}")
+            return super().form_invalid(form)
+        Tournament.results.set_points(Tournament.objects.get(id=tid))
+        msg = f"{form.instance.angler} Buy-in"
+        if not form.instance.buy_in:
+            msg = " ".join(
+                [
+                    f"{form.instance.angler} {form.instance.num_fish} fish",
+                    f"{form.instance.total_weight}lbs",
+                    f"{form.instance.big_bass_weight}lb BB",
+                ]
+            )
+        messages.success(self.request, f"Result added: {msg}")
+        return super().form_valid(form)
 
 
 #
 # Teams
 #
 class TeamCreateView(SuccessMessageMixin, LoginRequiredMixin, CreateView):
-    """Team creation view"""
-
     model = TeamResult
     form_class = TeamForm
     template_name = "tournaments/team_form.html"
 
-    def get_initial(self, *args, **kwargs):
-        """Get intital team create view"""
-        initial = super().get_initial(**kwargs)
+    def get_initial(self):
+        initial = super().get_initial()
         initial["tournament"] = self.kwargs.get("pk")
 
         return initial
 
     def test_func(self):
-        """Test function for values"""
         return any(
             [
                 self.request.user.angler.officer_type in OFFICERS,
@@ -218,40 +185,28 @@ class TeamCreateView(SuccessMessageMixin, LoginRequiredMixin, CreateView):
             ]
         )
 
-    def get_success_message(self, *args, **kwargs):
-        """Get success message"""
-        obj = self.get_object()
-        tournament_name = Tournament.objects.get(id=self.kwargs.get("pk")).name
-        return f"{str(obj)} added to {tournament_name}"
-
     def get_queryset(self):
-        """Get query set"""
         q_set = super().get_queryset()
         return q_set.filter(tournament=self.kwargs.get("pk"))
 
 
 class TeamListView(SuccessMessageMixin, LoginRequiredMixin, ListView):
-    """Team list view"""
-
     model = TeamResult
     form_class = TeamForm
     paginate_by = 10
     template_name = "tournaments/team_list.html"
     context_object_name = "teams"
 
-    def get_initial(self, *args, **kwargs):
-        """Get intitial view"""
-        initial = super().get_initial(**kwargs)
+    def get_initial(self):
+        initial = super().get_initial()
         initial["tournament"] = self.kwargs.get("pk")
         return initial
 
     def get_queryset(self):
-        """Get team queryset"""
         q_set = super().get_queryset()
         return q_set.filter(tournament=self.kwargs.get("pk"))
 
     def test_func(self):
-        """Test function for values"""
         return any(
             [
                 self.request.user.angler.officer_type in OFFICERS,
@@ -261,26 +216,21 @@ class TeamListView(SuccessMessageMixin, LoginRequiredMixin, ListView):
 
 
 class TeamDetailView(SuccessMessageMixin, LoginRequiredMixin, DetailView):
-    """Team detail view"""
-
     model = TeamResult
     form_class = TeamForm
     template_name = "tournaments/team_detail.html"
     context_object_name = "team"
 
-    def get_initial(self, *args, **kwargs):
-        """Get initial team"""
-        initial = super().get_initial(**kwargs)
+    def get_initial(self):
+        initial = super().get_initial()
         initial["tournament"] = self.kwargs.get("pk")
         return initial
 
     def get_queryset(self):
-        """Gets a queryset from a tournament object"""
         q_set = super().get_queryset()
         return q_set.filter(tournament=self.kwargs.get("pk"))
 
     def test_func(self):
-        """Test function for values"""
         return any(
             [
                 self.request.user.angler.officer_type in OFFICERS,
@@ -290,24 +240,19 @@ class TeamDetailView(SuccessMessageMixin, LoginRequiredMixin, DetailView):
 
 
 class TeamUpdateView(SuccessMessageMixin, LoginRequiredMixin, CreateView):
-    """Team update View"""
-
     model = Tournament
     form_class = TeamForm
     success_message = "%s Successfully Updated!"
 
-    def get_initial(self, *args, **kwargs):
-        """Get an intial TeamUpdateView state for a tournament"""
-        initial = super().get_initial(**kwargs)
+    def get_initial(self):
+        initial = super().get_initial()
         initial["tournament"] = self.kwargs.get("pk")
 
         return initial
 
     def test_func(self):
-        """Test that only officers can update team info"""
         return self.request.user.angler.type == "officer"
 
-    def save(self, *args, **kwargs):
-        """Save a team record"""
+    def save(self):
         obj = self.get_object()
         messages.success(self.request, self.success_message % obj.__dict__.get("name", "UNKNOWN"))
