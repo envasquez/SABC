95c94d17b029510c6b73bbb329cc6b5befbd50de
Eric Vasquez <vasquez.ericn@gmail.com>
2022-12-26 22:11:17 -0600

    Add new scema values to lakes from yaml

--- sabc/tournaments/admin.py
+++ sabc/tournaments/admin.py
@@ -7,30 +7,40 @@ from django.contrib import admin, messages
 from django.shortcuts import render
 
 from .forms import YamlImportForm
-from .models import Tournament, Result, TeamResult, RuleSet, Lake, Ramp
+from .models import (
+    Tournament,
+    Result,
+    TeamResult,
+    RuleSet,
+    Lake,
+    Ramp,
+    PayOutMultipliers,
+    TournamentPayOut,
+)
 
 
 def create_lake_from_yaml(request):
     lakes = safe_load(request.FILES["yaml_upload"])
     for lake_name in lakes:
-        lake, _ = Lake.objects.update_or_create(name=lake_name)
-        messages.info(request, f"Lake: {lake_name} Created Successfully!")
+        lake, _ = Lake.objects.update_or_create(
+            name=lake_name,
+            paper=lakes[lake_name].get("paper", False),
+            google_maps=lakes[lake_name].get("google_maps", ""),
+        )
         for ramp in lakes[lake_name]["ramps"]:
             result, _ = Ramp.objects.update_or_create(
                 lake=lake, name=ramp["name"], google_maps=ramp["google_maps"]
             )
-            messages.info(request, f"Ramp: {result.name} Created Successfully!")
+        messages.info(request, f"Lake: {lake_name} and all ramps reated Successfully!")
 
 
 class LakeAdmin(admin.ModelAdmin):
     def get_urls(self):
-        """Override of get_urls()"""
         urls = super().get_urls()
         new_urls = [path("upload-lakes/", self.upload_yaml)]
         return new_urls + urls
 
     def upload_yaml(self, request):
-        """YAML Upload handler"""
         form = YamlImportForm()
         data = {"form": form}
         if request.method == "POST":
@@ -39,4 +49,5 @@ class LakeAdmin(admin.ModelAdmin):
         return render(request, "admin/yaml_upload.html", data)
 
 
-admin.site.register([Tournament, Result, TeamResult, RuleSet, Lake, Ramp], admin_class=LakeAdmin)
+MODELS = [Tournament, Result, TeamResult, RuleSet, Lake, Ramp, PayOutMultipliers, TournamentPayOut]
+admin.site.register(MODELS, admin_class=LakeAdmin)
