6cb8a17ec4ba006bd159ee6dcc802ad2c3f568f7
Eric Vasquez <vasquez.ericn@gmail.com>
2023-01-18 17:41:20 -0600

    Fix annotations

--- sabc/polls/models.py
+++ sabc/polls/models.py
@@ -1,41 +1,38 @@
 # -*- coding: utf-8 -*-
-from typing import Type
-
 from time import strftime
 
 from django.db.models import (
-    Model,
     CASCADE,
-    DateField,
     CharField,
-    TextField,
+    DateField,
     ForeignKey,
     ManyToManyField,
+    Model,
+    TextField,
 )
-
-from users.models import Angler
 from tournaments.models.lakes import Lake
+from users.models import Angler
 
 CURRENT_YEAR: str = f"{strftime('%Y')}"
 CURRENT_MONTH: str = f"{strftime('%B')}"
 
 
 class LakePoll(Model):
-    name: Type[CharField] = CharField(
+    name: CharField | str = CharField(
         default=f"Poll: {CURRENT_MONTH} {CURRENT_YEAR}", max_length=256
     )
-    choices: Type[ManyToManyField] = ManyToManyField(Lake)
-    description: Type[TextField] = TextField(max_length=4096, null=True, blank=True)
+    choices: ManyToManyField = ManyToManyField(Lake)
+    description: TextField | str = TextField(max_length=4096, null=True, blank=True)
 
     def __str__(self) -> str:
         return f"{self.name}"
 
 
 class LakeVote(Model):
-    poll: Type[ForeignKey] = ForeignKey(LakePoll, on_delete=CASCADE)
-    choice: Type[ForeignKey] = ForeignKey(Lake, on_delete=CASCADE)
-    angler: Type[ForeignKey] = ForeignKey(Angler, on_delete=CASCADE)
-    timestamp: Type[DateField] = DateField(auto_now=True)
+    poll: ForeignKey = ForeignKey(LakePoll, on_delete=CASCADE)
+    choice: ForeignKey = ForeignKey(Lake, on_delete=CASCADE)
+    angler: ForeignKey = ForeignKey(Angler, on_delete=CASCADE)
+    timestamp: DateField = DateField(auto_now=True)
 
     def __str__(self) -> str:
         return f"{self.poll}: {self.choice} {self.angler}"
