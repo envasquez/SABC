dd56709b97751fd9c4b3e4282a5911c7cc2c7aee
Eric Vasquez <env@erics-mbp.lan>
2023-01-24 20:55:04 -0600

    Add more result validation

--- sabc/tournaments/views/results.py
+++ sabc/tournaments/views/results.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from decimal import Decimal
+
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.messages.views import SuccessMessageMixin
@@ -7,7 +9,7 @@ from django.views.generic import CreateView, DeleteView, UpdateView
 
 from ..forms import ResultForm, TeamForm
 from ..models.results import Result, TeamResult
-from ..models.tournaments import Tournament, set_places  # , set_points
+from ..models.tournaments import Tournament
 
 
 class ResultCreateView(SuccessMessageMixin, LoginRequiredMixin, UserPassesTestMixin, CreateView):
@@ -30,23 +32,17 @@ class ResultCreateView(SuccessMessageMixin, LoginRequiredMixin, UserPassesTestMi
     def form_valid(self, form):
         tid = self.kwargs.get("pk")
         duplicate = form.instance.angler in [r.angler for r in Result.objects.filter(tournament=tid)]
-        if duplicate:  # Don't add duplicate records!
-            result: Result = Result.objects.get(tournament=tid, angler=form.instance.angler)
-            messages.error(self.request, message=f"ERROR Result exists! {result}")
+        zero_fish_wt = form.instance.num_fish == 0 and form.instance.total_weight > Decimal("0")
+        errors = any([duplicate, zero_fish_wt])
+        if duplicate:
+            messages.error(self.request, message=f"ERROR Result exists for {form.instance.angler} ... edit instead?")
+        elif zero_fish_wt:
+            messages.error(self.request, message=f"ERROR Result exists for {form.instance.angler} ... edit instead?")
+        if errors:
             return super().form_invalid(form)
-
-        set_places(tid=tid)
-        msg = f"{form.instance.angler} Buy-in"
-        if not form.instance.buy_in:
-            msg = " ".join(
-                [
-                    f"{form.instance.angler} {form.instance.num_fish} fish",
-                    f"{form.instance.total_weight}lbs",
-                    f"{form.instance.big_bass_weight}lb BB",
-                ]
-            )
-        messages.success(self.request, f"Result added: {msg}")
+        messages.success(self.request, f"Result added: {form.instance}")
         return super().form_valid(form)
+
 
 class ResultUpdateView(SuccessMessageMixin, LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Result
@@ -63,6 +59,14 @@ class ResultUpdateView(SuccessMessageMixin, LoginRequiredMixin, UserPassesTestMi
     def get_success_url(self):
         return reverse_lazy("tournament-details", kwargs={"pk": self.get_object().tournament.id})
 
+    def form_valid(self, form):
+        result = form.instance
+        if result.total_weight > 0 and result.num_fish == 0:
+            msg = f"ERROR Can't have weight: {result.total_weight}lbs with {result.num_fish} fish weighed "
+            msg += "please fix the field: Num fish!"
+            messages.error(self.request, message=msg)
+            return super().form_invalid(form)
+        return super().form_valid(form)
 
 class ResultDeleteView(SuccessMessageMixin, LoginRequiredMixin, UserPassesTestMixin, DeleteView):  # type: ignore
     model = Result
@@ -104,7 +108,7 @@ class TeamCreateView(SuccessMessageMixin, LoginRequiredMixin, UserPassesTestMixi
     def form_valid(self, form):
         tid = self.get_initial()["tournament"]
         results = TeamResult.objects.filter(tournament=tid)
-        anglers = [r.result_1.angler for r in results] + [r.result_2.angler for r in results]
+        anglers = [r.result_1.angler for r in results] + [r.result_2.angler for r in results if r.result_2]
         err = "Team Result for %s already exists!"
         if form.instance.result_1.angler in anglers:
             messages.error(self.request, err % form.instance.result_1.angler)
@@ -113,7 +117,9 @@ class TeamCreateView(SuccessMessageMixin, LoginRequiredMixin, UserPassesTestMixi
             if form.instance.result_2.angler in anglers:
                 messages.error(self.request, err % form.instance.result_2.angler)
                 return self.form_invalid(form)
-
+            if form.instance.result_2.angler == form.instance.result_1.angler:
+                messages.error(self.request, "Angler 2 cannot be the same as Angler 1!")
+                return self.form_invalid(form)
         msg = f"{form.instance.result_1.angler}"
         msg += f"& {form.instance.result_2.angler}" if form.instance.result_2 else " - solo"
         messages.success(self.request, f"Team added: {msg}")
