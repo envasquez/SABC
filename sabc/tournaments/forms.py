a492deaf4576022522875724916b396de8ec724c
Eric Vasquez <vasquez.ericn@gmail.com>
2023-01-28 11:41:51 -0600

    fix #115 - Remove paper option, it gets set based on the lake automatically

--- sabc/tournaments/forms.py
+++ sabc/tournaments/forms.py
@@ -10,7 +10,7 @@ class TournamentForm(ModelForm):
     class Meta:
         model = Tournament
         fields = "__all__"
-        exclude = ("created_by", "updated_by")
+        exclude = ("created_by", "updated_by", "paper")
 
 
 class EventForm(ModelForm):
