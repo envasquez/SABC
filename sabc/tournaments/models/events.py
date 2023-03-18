bfe3db6f67d925f5dbbe510f6089decb0896e388
Eric Vasquez <vasquez.ericn@gmail.com>
2023-03-18 18:30:15 -0500

    Fix the get next event function

--- sabc/tournaments/models/events.py
+++ sabc/tournaments/models/events.py
@@ -2,28 +2,12 @@
 import datetime
 
 import pytz
-from django.db.models import (
-    CharField,
-    DateField,
-    Model,
-    QuerySet,
-    SmallIntegerField,
-    TextChoices,
-    TimeField,
-)
+from django.db.models import CharField, DateField, Model, QuerySet, SmallIntegerField, TextChoices, TimeField
 
-DEFAULT_MEETING_START: datetime.time = datetime.datetime.time(
-    datetime.datetime.strptime("7:00 pm", "%I:%M %p")
-)
-DEFAULT_MEETING_FINISH: datetime.time = datetime.datetime.time(
-    datetime.datetime.strptime("8:00 pm", "%I:%M %p")
-)
-DEFAULT_TOURNAMENT_START: datetime.time = datetime.datetime.time(
-    datetime.datetime.strptime("12:00 am", "%I:%M %p")
-)
-DEFAULT_TOURNAMENT_FINISH: datetime.time = datetime.datetime.time(
-    datetime.datetime.strptime("12:00 am", "%I:%M %p")
-)
+DEFAULT_MEETING_START: datetime.time = datetime.datetime.time(datetime.datetime.strptime("7:00 pm", "%I:%M %p"))
+DEFAULT_MEETING_FINISH: datetime.time = datetime.datetime.time(datetime.datetime.strptime("8:00 pm", "%I:%M %p"))
+DEFAULT_TOURNAMENT_START: datetime.time = datetime.datetime.time(datetime.datetime.strptime("12:00 am", "%I:%M %p"))
+DEFAULT_TOURNAMENT_FINISH: datetime.time = datetime.datetime.time(datetime.datetime.strptime("12:00 am", "%I:%M %p"))
 
 
 class Events(Model):
@@ -50,9 +34,7 @@ class Events(Model):
         DECEMBER: str = "december"
 
     date: DateField = DateField(null=True, blank=True)
-    type: CharField = CharField(
-        choices=EventTypes.choices, default="tournament", max_length=25
-    )
+    type: CharField = CharField(choices=EventTypes.choices, default="tournament", max_length=25)
     year: SmallIntegerField = SmallIntegerField(default=datetime.date.today().year)
     month: CharField = CharField(choices=Months.choices, max_length=20)
     start: TimeField = TimeField(null=True, blank=True)
@@ -87,34 +69,20 @@ def get_next_event(event_type: str, today: datetime.date) -> Events | None:
     """Return the next event, relative to 'today'"""
     if not Events.objects.filter(type=event_type):
         return None
-    events: QuerySet = Events.objects.filter(type=event_type, year=today.year).order_by(
-        "date"
-    )
+
+    events: QuerySet = Events.objects.filter(type=event_type, year=today.year).order_by("date")
+    now: datetime.datetime = datetime.datetime.now(pytz.timezone("US/Central"))
+    if now.month == 12 and now.day > 15:
+        return Events.objects.filter(type=event_type, year=today.year + 1).order_by("date").first()
+
     next_event: Events | None = None
     for idx, event in enumerate(events):
-        current_month: bool = event.date.month == today.month
-        current_year: bool = event.year == today.year
-        now: datetime.datetime = datetime.datetime.now(pytz.timezone("US/Central"))
-        if all(
-            [
-                current_year,
-                current_month,
-                now.day <= event.date.day,
-                now.hour <= event.start.hour,
-            ]
-        ):
+        if now.month != event.date.month:
+            continue
+        if now.day <= event.date.day and now.hour <= event.start.hour:
             next_event = event
         else:
-            next_event = events[idx + 1] if idx + 1 < events.count() else None
-        if (
-            today.month == 12
-        ):  # Last event of the year, get the first event for next year ...
-            next_event = (
-                Events.objects.filter(type=event_type, year=today.year + 1)
-                .order_by("date")
-                .first()
-            )
-        if next_event:
-            break
+            next_event = events[idx + 1]
+        break
 
     return next_event
--- sabc/tournaments/tests/test_events.py
+++ sabc/tournaments/tests/test_events.py
@@ -9,17 +9,9 @@ from ..models.events import Events, get_next_event
 @pytest.mark.django_db
 def test_get_next_event() -> None:
     now = datetime.datetime.now()
-    event_1_date: datetime.date = datetime.date(
-        year=now.year, month=now.month, day=now.day + 1
-    )
-    next_mtg: Events = Events.objects.create(type="meeting", date=event_1_date)
-    event_2_date: datetime.date = datetime.date(
-        year=now.year, month=now.month + 1, day=now.day
-    )
-    Events.objects.create(type="meeting", date=event_2_date)
-    next_event = get_next_event(event_type="meeting", today=datetime.date.today())
+    for month in range(1, 13):
+        event_date: datetime.date = datetime.date(year=now.year, month=month, day=3)
+        Events.objects.create(type="meeting", date=event_date)
 
-    assert next_mtg == next_event
-    assert next_mtg.date.day == next_event.date.day
-    assert next_mtg.date.month == next_event.date.month
-    assert next_mtg.date.year == next_event.date.year
+    next_event = get_next_event(event_type="meeting", today=datetime.date.today())
+    assert next_event.date.month >= now.month
