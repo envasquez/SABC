1a959fad8a0b7731332bb34fc66ce83eb880b3b4
Eric Vasquez <vasquez.ericn@gmail.com>
2025-09-03 18:19:06 -0500

    Post phase completion changes

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -5,17 +5,17 @@ import os
 bind = f"{os.environ.get('HOST', '0.0.0.0')}:{os.environ.get('PORT', '8000')}"
 
 # Worker processes
-workers = int(os.environ.get('WORKERS', '4'))
-worker_class = 'uvicorn.workers.UvicornWorker'
+workers = int(os.environ.get("WORKERS", "4"))
+worker_class = "uvicorn.workers.UvicornWorker"
 
 # Logging
 access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" %(D)s'
-accesslog = '-'  # Log to stdout
-errorlog = '-'   # Log to stderr
-loglevel = 'info'
+accesslog = "-"  # Log to stdout
+errorlog = "-"  # Log to stderr
+loglevel = "info"
 
 # Process naming
-proc_name = 'sabc-tournament'
+proc_name = "sabc-tournament"
 
 # Worker timeout (30 seconds)
 timeout = 30
