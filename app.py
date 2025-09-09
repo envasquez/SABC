f833b63c0fe643ff0aed6e738f5bfea00dfc1adc
Eric Vasquez <vasquez.ericn@gmail.com>
2025-09-09 06:22:30 -0500

    Gave CC a mandate to format/lint code before pushing

--- app.py
+++ app.py
@@ -1,36 +1,17 @@
-import json
 import os
-from datetime import date, datetime, timedelta
-from typing import Optional
 
-import bcrypt
 import uvicorn
-from fastapi import FastAPI, Form, HTTPException, Query, Request
-from fastapi.responses import JSONResponse, RedirectResponse, Response
+from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from sqlalchemy import text
 from starlette.middleware.sessions import SessionMiddleware
 
-from core.auth_helpers import admin, u
-from core.database import db
 from core.filters import (
     date_format_filter,
     from_json_filter,
     month_number_filter,
     time_format_filter,
 )
-from core.lakes import (
-    find_lake_by_id,
-    find_lake_data_by_db_name,
-    find_ramp_name_by_id,
-    get_all_ramps,
-    get_lakes_list,
-    get_ramps_for_lake,
-    load_lakes_data,
-    validate_lake_ramp_combo,
-)
-from core.validators import get_federal_holidays, validate_event_data
 
 app = FastAPI(redirect_slashes=False)
 app.add_middleware(
